78601fd7cf66845ce7f8582b7159010a2d25ca36
MohaAarab <mohamed@aarabautomation.com>
2026-09-01 18:12:42 +0200

    variables and types

--- Variables_and_types/challenge12.js
+++ Variables_and_types/challenge12.js
@@ -3,8 +3,8 @@ const prompt = require("prompt-sync")()
 let nb=Number(prompt('enter the nb : '))
 
 const a=Math.floor(nb%10)
-const b=Math.floor(nb/100)%10
 const c=Math.floor(nb/10)%10
+const b=Math.floor(nb/100)%10
 const d=Math.floor(nb/1000)
 
 console.log(a,c,b,d)
